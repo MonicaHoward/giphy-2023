1f23e11a957770e3928f7c747286b785218721c1
Monica <jay.m.howard@gmail.com>
2023-05-31 21:02:54 -0500

    added search feature

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const gifContainerEl = document.querySelector('#gif-container');
+let searchBoxEl = document.querySelector('#search-box');
+const searchButtonEl = document.querySelector('#search-button');
+
+
 
 function getGifs() {
 
-    fetch('https://api.giphy.com/v1/gifs/trending?api_key=rlGjlbEQibLMHv9podBx98qqxWvMQjFB&limit=10&rating="g"')
+    let searchTerm = searchBoxEl.value;
+    console.log(searchTerm);
+
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=rlGjlbEQibLMHv9podBx98qqxWvMQjFB&limit=10&rating="g"&q=' + searchTerm +'')
         .then((response) => response.json())
         .then((data) => {
 
             const results = data.data;
 
-            console.log(results);
+            // console.log(results);
 
             results.map(gif => {
                 let title = document.createElement('h4');
@@ -17,7 +24,7 @@ function getGifs() {
                 title.textContent = gif.title;
                 giphyBox.innerHTML = `<img src="${gif.images.original.url}" alt="${gif.title}" />`
 
-                console.log(giphyBox);
+                // console.log(giphyBox);
 
                 gifContainerEl.append(title);
                 gifContainerEl.append(giphyBox);
@@ -26,4 +33,5 @@ function getGifs() {
         })
 }
 
-getGifs();
+
+
